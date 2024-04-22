f071051af23d386b894e95713c8009021fd71208
lilly-lizard <bigbanana29@aol.com>
2024-04-22 21:35:48 +1200

    subfunctions done

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ const SETTING_NAME_SHIFT: &str = "shift";
 const SETTING_NAME_CONTROL: &str = "control";
 const SETTING_NAME_ALT: &str = "alt";
 
+type JsonSettings = serde_json::Map<String, serde_json::Value>;
+
 #[derive(PartialEq, Eq, Clone, Copy)]
 enum KeyboardModifier {
     Shift,
@@ -138,7 +140,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
-    let serde_json::Value::Object(mut settings_map) = settings_loaded else {
+    let serde_json::Value::Object(mut all_settings) = settings_loaded else {
         panic!(
             "no json parent object. make sure there are curly brackets at the start and end of
             the file (even if there are no settings)"
@@ -149,136 +151,185 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     update_camera_control_mappings_from_json_settings(
         &mut camera_control_mappings,
-        &mut settings_map,
+        &mut all_settings,
     );
 
+    // for everything left -> warning
+    warn_invalid_remaining_settings(all_settings);
+
     Ok(())
+}
+
+/// Logs a warning for any settings remaining in `remaining_json_settings`
+fn warn_invalid_remaining_settings(remaining_json_settings: JsonSettings) {
+    for (setting_name, _setting_value) in remaining_json_settings {
+        println!("invalid setting: {}", setting_name);
+    }
 }
 
 /// Consumes any settings used in `json_settings`
 fn update_camera_control_mappings_from_json_settings(
     camera_control_mappings: &mut CameraControlMappings,
-    json_settings: &mut serde_json::Map<String, serde_json::Value>,
+    json_settings: &mut JsonSettings,
 ) {
     // look camera mapping
-    if let Some(possible_camera_look_setting) = json_settings.remove(SETTING_NAME_LOOK_MAPPING) {
-        if let serde_json::Value::Object(camera_json_settings) = possible_camera_look_setting {
-            let mouse_mapping = get_mouse_mapping_from_json_settings(
-                camera_json_settings,
-                SETTING_NAME_LOOK_MAPPING,
-            );
-            camera_control_mappings.look = mouse_mapping;
-        } else {
-            println!(
-                "invalid format for camera control setting: {}",
-                SETTING_NAME_LOOK_MAPPING
-            );
-        }
+    if let Some(mouse_mapping) =
+        parse_mouse_mapping_setting(json_settings, &SETTING_NAME_LOOK_MAPPING)
+    {
+        camera_control_mappings.look = mouse_mapping;
+    }
+    if let Some(mouse_mapping) =
+        parse_mouse_mapping_setting(json_settings, &SETTING_NAME_LOOK_MAPPING_2)
+    {
+        camera_control_mappings.look_2 = Some(mouse_mapping);
     }
 
-    // 2nd look camera mapping
-    if let Some(possible_camera_look_setting) = json_settings.remove(SETTING_NAME_LOOK_MAPPING_2) {
-        if let serde_json::Value::Object(camera_json_settings) = possible_camera_look_setting {
-            let mouse_mapping = get_mouse_mapping_from_json_settings(
-                camera_json_settings,
-                SETTING_NAME_LOOK_MAPPING_2,
-            );
-            camera_control_mappings.look_2 = Some(mouse_mapping);
-        } else {
-            println!(
-                "invalid format for camera control setting: {}",
-                SETTING_NAME_LOOK_MAPPING_2
-            );
-        }
+    // pan camera mapping
+    if let Some(mouse_mapping) =
+        parse_mouse_mapping_setting(json_settings, &SETTING_NAME_PAN_MAPPING)
+    {
+        camera_control_mappings.pan = mouse_mapping;
+    }
+    if let Some(mouse_mapping) =
+        parse_mouse_mapping_setting(json_settings, &SETTING_NAME_PAN_MAPPING_2)
+    {
+        camera_control_mappings.pan_2 = Some(mouse_mapping);
+    }
+
+    // zoom camera mapping
+    if let Some(mouse_mapping) =
+        parse_mouse_mapping_setting(json_settings, &SETTING_NAME_ZOOM_MAPPING)
+    {
+        camera_control_mappings.zoom = mouse_mapping;
+    }
+    if let Some(mouse_mapping) =
+        parse_mouse_mapping_setting(json_settings, &SETTING_NAME_ZOOM_MAPPING_2)
+    {
+        camera_control_mappings.zoom_2 = Some(mouse_mapping);
     }
 }
 
-fn parse_mouse_mapping(
-    json_settings: &mut serde_json::Map<String, serde_json::Value>,
-    camera_control_mappings: &mut CameraControlMappings,
+fn parse_mouse_mapping_setting(
+    json_settings: &mut JsonSettings,
     mapping_setting_name: &'static str,
 ) -> Option<MouseMapping> {
     if let Some(possible_camera_look_setting) = json_settings.remove(mapping_setting_name) {
         if let serde_json::Value::Object(camera_json_settings) = possible_camera_look_setting {
-            let mouse_mapping =
-                get_mouse_mapping_from_json_settings(camera_json_settings, mapping_setting_name);
-            return Some(mouse_mapping);
-        } else {
-            println!(
-                "invalid format for camera control setting: {}",
-                mapping_setting_name
+            return get_mouse_mapping_from_mapping_settings(
+                camera_json_settings,
+                mapping_setting_name,
             );
         }
+        println!(
+            "invalid format for camera control setting: {}",
+            mapping_setting_name
+        );
     }
     None
 }
 
-fn get_mouse_mapping_from_json_settings(
-    mut camera_json_settings: serde_json::Map<String, serde_json::Value>,
+fn get_mouse_mapping_from_mapping_settings(
+    mut mapping_settings: JsonSettings,
     camera_json_setting_name: &'static str,
-) -> MouseMapping {
+) -> Option<MouseMapping> {
     let mut mouse_mapping = MouseMapping::default();
 
-    println!("mention {} in all warnings...", camera_json_setting_name);
+    // todo test each warning
+    println!(
+        "todo mention {} in all warnings...",
+        camera_json_setting_name
+    );
 
     // mouse button
-    if let Some(possible_mouse_button_setting) =
-        camera_json_settings.remove(SETTING_NAME_MOUSE_BUTTON)
-    {
-        if let serde_json::Value::String(mouse_button_string) = possible_mouse_button_setting {
-            if let Some(mouse_button) = MouseButton::from_setting_name(&mouse_button_string) {
-                mouse_mapping.mouse_button = mouse_button
-            } else {
-                println!(
-                    "invalid {} property: {}",
-                    SETTING_NAME_MOUSE_BUTTON, mouse_button_string
-                );
-            }
-        } else {
-            println!("invalid format for {} setting", SETTING_NAME_MOUSE_BUTTON);
-        }
+    if let Some(mouse_button) = get_mouse_button_from_mapping_settings(&mut mapping_settings) {
+        mouse_mapping.mouse_button = mouse_button;
+    } else {
+        println!(
+            "error: a mouse mapping must include a {} value",
+            SETTING_NAME_MOUSE_BUTTON
+        );
+        return None;
     }
 
     // modifiers
-    if let Some(possible_modifiers_setting) = camera_json_settings.remove(SETTING_NAME_MODIFIERS) {
+    if let Some(possible_modifiers_setting) = mapping_settings.remove(SETTING_NAME_MODIFIERS) {
         if let serde_json::Value::Array(modifiers_array) = possible_modifiers_setting {
-            let mut modifier_index: usize = 0;
-            for modifier_setting in modifiers_array {
-                if let serde_json::Value::String(modifier_string) = modifier_setting {
-                    if let Some(modifier) = KeyboardModifier::from_setting_name(&modifier_string) {
-                        if mouse_mapping.modifiers.contains(&Some(modifier)) {
-                            println!(
-                                "duplicate modifier found for {} setting: {}",
-                                camera_json_setting_name, modifier
-                            );
-                        } else {
-                            // insert unique modifier
-                            if modifier_index >= MAX_MODIFIERS {
-                                println!("there can only be maximum of {} unique modifiers per mouse mapping", MAX_MODIFIERS);
-                                continue;
-                            }
-                            mouse_mapping.modifiers[modifier_index] = Some(modifier);
-                            modifier_index = modifier_index + 1;
-                        }
-                    } else {
-                        println!("invalid keyboard modifier name: {}", modifier_string);
-                    }
-                } else {
-                    println!(
-                        "invalid property found in {} array: {}",
-                        SETTING_NAME_MODIFIERS, modifier_setting
-                    );
-                }
-            }
+            set_mouse_mapping_modifiers_from_mapping_settings(
+                modifiers_array,
+                &mut mouse_mapping,
+                camera_json_setting_name,
+            );
         } else {
             println!("invalid format for {} setting", SETTING_NAME_MODIFIERS);
         }
     }
 
     // remaining json values are invalid
-    for (json_string, _json_value) in camera_json_settings {
+    for (json_string, _json_value) in mapping_settings {
         println!("invalid property: {}", json_string);
     }
 
-    mouse_mapping
+    Some(mouse_mapping)
+}
+
+fn get_mouse_button_from_mapping_settings(
+    mapping_settings: &mut serde_json::Map<String, serde_json::Value>,
+) -> Option<MouseButton> {
+    if let Some(possible_mouse_button_setting) = mapping_settings.remove(SETTING_NAME_MOUSE_BUTTON)
+    {
+        if let serde_json::Value::String(mouse_button_string) = possible_mouse_button_setting {
+            if let Some(mouse_button) = MouseButton::from_setting_name(&mouse_button_string) {
+                return Some(mouse_button);
+            }
+
+            println!(
+                "invalid {} property: {}",
+                SETTING_NAME_MOUSE_BUTTON, mouse_button_string
+            );
+        } else {
+            println!("invalid format for {} setting", SETTING_NAME_MOUSE_BUTTON);
+        }
+    }
+    None
+}
+
+fn set_mouse_mapping_modifiers_from_mapping_settings(
+    modifiers_array: Vec<serde_json::Value>,
+    mouse_mapping: &mut MouseMapping,
+    camera_json_setting_name: &str,
+) {
+    let mut modifier_index: usize = 0;
+
+    for modifier_setting in modifiers_array {
+        if modifier_index >= MAX_MODIFIERS {
+            println!(
+                "there can only be maximum of {} unique modifiers per mouse mapping",
+                MAX_MODIFIERS
+            );
+            return;
+        }
+
+        if let serde_json::Value::String(modifier_string) = modifier_setting {
+            if let Some(modifier) = KeyboardModifier::from_setting_name(&modifier_string) {
+                if mouse_mapping.modifiers.contains(&Some(modifier)) {
+                    println!(
+                        "duplicate modifier found for {} setting: {}",
+                        camera_json_setting_name, modifier
+                    );
+                    continue;
+                }
+
+                // insert unique modifier
+                mouse_mapping.modifiers[modifier_index] = Some(modifier);
+                modifier_index = modifier_index + 1;
+            } else {
+                println!("invalid keyboard modifier name: {}", modifier_string);
+            }
+        } else {
+            println!(
+                "invalid property found in {} array: {}",
+                SETTING_NAME_MODIFIERS, modifier_setting
+            );
+        }
+    }
 }
